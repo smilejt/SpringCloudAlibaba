47a3f0b1ae2f2cfffeb6e385ae69ab65de6a77fb
LongJunTao <1997640@dingtalk.com>
2019-07-23 16:28:25 +0800

    使用nacos线上配置

---
+++ jt-provider/src/main/java/com/abandon/jt/provider/ProviderApplication.java
@@ -0,0 +1,36 @@
+package com.abandon.jt.provider;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @createData: 2019-07-23 13:59
+ * @author: LongJunTao
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProviderApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProviderApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+        logger.info("---------------------Provider启动成功---------------------");
+    }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{message}")
+        public String echo(@PathVariable String message) {
+            return "Hello Nacos Discovery " + message;
+        }
+    }
+}
