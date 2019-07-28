48e6541e1c132436c2d3f0c4ce2c0e33d62c652e
ly <smile_jt@qq.com>
2019-07-28 20:00:52 +0800

    1.Nacos配置数据库连接获取不到

--- jt-auth/src/main/java/com/abandon/jt/auth/service/Impl/AuthServiceImpl.java
+++ jt-auth/src/main/java/com/abandon/jt/auth/service/Impl/AuthServiceImpl.java
@@ -11,7 +11,6 @@ import com.abandon.jt.common.utils.JsonUtils;
 import com.alibaba.nacos.client.naming.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
